87d3c37e4f72cb9b385ce2e95c4b4c73b0573a93
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2024-01-16 15:55:49 +0100

    Initial commit

---
+++ src/test/java/com/example/rabbitmqservices/RabbitMqServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
